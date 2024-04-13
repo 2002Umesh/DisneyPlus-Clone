c8c0a1956c3c2e66c7f63820b083fc3241a691ef
Umesh Das <dasumesh2002@gmail.com>
2024-04-14 00:29:59 +0530

    s

--- my-server/server.js
+++ my-server/server.js
@@ -12,18 +12,18 @@ const errorMiddleware = require("./middlewares/error-middleware.js");
 
 //middleware
 // Enable CORS for all requests
-// app.use(function (req, res, next) {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "https://disney-plus-clone-front.vercel.app"
-//   );
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://disney-plus-clone-front.vercel.app"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 //handling cors policy and hosting
 const corsOptions = {
   origin: [
