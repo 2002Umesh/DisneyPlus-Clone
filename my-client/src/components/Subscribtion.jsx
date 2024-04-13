02d1ee563793f06b87a93e49cc0eca69d028c3da
Umesh Das <dasumesh2002@gmail.com>
2024-04-14 00:10:43 +0530

    l

--- my-client/src/components/Subscribtion.jsx
+++ my-client/src/components/Subscribtion.jsx
@@ -19,7 +19,7 @@ function Subscribtion() {
     const {
       data: { key },
     } = await axios.get(
-      `${BACKURL}/api/payment/rzp_test_T2Lny50ZRfp6vd`,
+      `${BACKURL}/api/payment/getkey`,
       { withCredentials: true }
       // , {
       //   withCredentials: true,
