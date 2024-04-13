24a43a9c7bdfad25d448fa7c3d51a118ba7e1332
Umesh Das <dasumesh2002@gmail.com>
2024-04-14 00:33:51 +0530

    d

--- my-server/server.js
+++ my-server/server.js
@@ -17,7 +17,7 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Origin",
     "https://disney-plus-clone-front.vercel.app"
   );
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
